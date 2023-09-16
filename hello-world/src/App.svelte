4f43f8eabf7a9789911a01de5d387a91368b5d39
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-16 21:18:03 +0530

    add demo

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
 	const name = "Samadhi";
+	const channel = '<b>samadhi</b>';
+	const hack = `<a href="#" onclick="alert('you have been hacked')" >Win a prize!</a>`;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<div>{@html channel}</div>
+	<div>{@html hack}</div>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
