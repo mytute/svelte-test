c6135a7d4ebe9f688a3f6d01db7f34c71c6b4b28
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-18 00:13:13 +0530

    add test

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,19 +1,9 @@
 <script>
-	const names = ['Bruce', 'Clark', 'Diana'];
-	const fullNames = [
-		{first:'Bruce', last: 'Wayne'},
-		{first:'Clark', last: 'Kent'},
-		{first:'Princess', last: 'Diana'},
-	];
+
 </script>
 
 <main>
- {#each names as name, index}
-    <h2>{index+1} {name}</h2>
- {/each}
- {#each fullNames as name, index (name.first)}
-    <h2>{index+1} {name.first} {name.last}</h2>
-  {/each}
+
 </main>
 
 <style>
