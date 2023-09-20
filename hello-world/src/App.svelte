7ab87cc9dfe81e4920820ba297aa1716a0b031c6
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-20 23:38:22 +0530

    add demo

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,8 +1,39 @@
 <script>
+	import { onMount } from "svelte";
+	import { onDestroy } from "svelte";
+	import { beforeUpdate, afterUpdate } from "svelte";
+	import { tick } from "svelte";
+	import PostList from "./components/PostList.svelte";
+	import AutoFocus from "./components/AutoFocus.svelte";
+
+	onMount(async ()=>{
+		console.log("onMount");
+		// call api here
+	});
+
+	const interval = setInterval(()=>{}, 1000);
+
+	onDestroy(()=>{
+		console.log("onDestroy");
+		clearInterval(interval)
+	});
+
+	let div, autoscroll;
+	beforeUpdate(()=>{
+		console.log("beforeUpdate");
+		autoscroll =div && (div.offsetHeight + div.scrollTop) > (div.scrollHeight -20);
+	})
+
+	afterUpdate(()=>{
+		console.log("afterUpdate");
+		if(autoscroll) div.scrollTo(0, div.scrollHeight);
+	})
 
 </script>
 
 <main>
+	<PostList/>
+	<AutoFocus/>
 
 </main>
 
---
+++ hello-world/src/components/AutoFocus.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from "svelte";
+  let inputRef
+
+  onMount(()=>{
+    inputRef.focus();
+  })
+</script>
+
+<main>
+    <input type="text" bind:this={inputRef} >
+</main>
