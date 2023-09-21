2652b4293ecad2f264b1da5d819f823964df5f98
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-21 10:04:22 +0530

    add demo

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,9 +1,12 @@
 <script>
-
+  import Counter, { getTotalCount }  from "./components/Counter.svelte";
 </script>
 
 <main>
-
+	<button on:click={()=>{alert(getTotalCount())}} >Alert total count</button>
+   <Counter/>
+   <Counter/>
+   <Counter/>
 </main>
 
 <style>
---
+++ hello-world/src/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    let totalCount = 0;
+    export function getTotalCount(){
+        return totalCount;
+    }
+</script>
+
+<script>
+    let counter = 0;
+    function clickHandler(event){
+        counter ++;
+        totalCount ++;
+    }
+</script>
+<main>
+    <h2>Count - {totalCount}</h2>
+    <button on:click={clickHandler}>Increment</button>
+</main>
